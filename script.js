82a919067fb455cd509e777e5d3bb51664ef62d4
Nergiz Şengün <nergizduygu56@gmail.com>
2025-01-31 16:34:33 +0300

    cevaplama süresi ekleme

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const ui = new UI();
 
  ui.btn_start.addEventListener("click", function() {
     ui.quiz_box.classList.add("active");
+    startTimer(10);
     ui.soruGoster(quiz.soruGetir());
     ui.soruSayisiniGoster(quiz.soruIndex + 1, quiz.sorular.length);
     ui.btn_next.classList.remove("show");
@@ -13,11 +14,13 @@ const ui = new UI();
 ui.btn_next.addEventListener("click", function(){
     if (quiz.sorular.length != quiz.soruIndex +1) {
         quiz.soruIndex += 1;
+        clearInterval(counter);
+        startTimer(10);
         ui.soruGoster(quiz.soruGetir());
         ui.soruSayisiniGoster(quiz.soruIndex + 1, quiz.sorular.length);
         ui.btn_next.classList.remove("show");
     } else {
-        console.log("quiz bitti");
+        clearInterval(counter);
         ui.quiz_box.classList.remove("active");
         ui.score_box.classList.add("active");
         ui.skoruGoster(quiz.sorular.length, quiz.dogruCevapSayisi);
@@ -40,9 +43,9 @@ ui.btn_replay.addEventListener("click", function(){
 
 
 function optionSelected(option){
+    clearInterval(counter);
     let cevap = option.querySelector("span b").textContent;
     let soru = quiz.soruGetir();
-
 
     if(soru.cevabiKontrolEt(cevap)){
         quiz.dogruCevapSayisi += 1;
@@ -62,3 +65,31 @@ function optionSelected(option){
 
 }
 
+let counter;
+function startTimer(time){
+   counter=  setInterval(timer, 1000);
+
+    function timer(){
+        ui.time_second.textContent = time;
+        time--;
+
+        if(time < 0){
+            clearInterval(counter);
+
+            ui.time_text.textContent = "Süre Bitti";
+
+            let cevap = quiz.soruGetir().dogruCevap;
+
+            for(let option of ui.option_list.children){
+                if(option.querySelector("span b").textContent == cevap){
+                    option.classList.add("correct");
+                    option.insertAdjacentElement("beforeend", ui.correctIcon);
+                }
+
+                option.classList.add("disabled");
+            }
+
+            ui.btn_next.clas
+        }
+    }
+}
